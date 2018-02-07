d7e6a9b0e1e0f98da27586f60790bb8d6711aa62
Pi4IoT <36192933+Pi4IoT@users.noreply.github.com>
2018-02-07 21:40:38 +0100

    Update sdCardBoot.sh

--- sdCardBoot.sh
+++ sdCardBoot.sh
@@ -1,8 +1,7 @@
-#!/bin/bash 
-#
-# Script for prepare booting from an SD Card
-# Pi4IoT - 06/01/2018
-#
+#------------------------------------------------
+# Script for prepare booting from an SD Card-----
+# Pi4IoT - 06/01/2018----------------------------
+#------------------------------------------------
 
 yellow='\e[0;33m'
 yellow_bold='\e[0;1;33m'
@@ -12,26 +11,26 @@ normal='\e[0m'
 
 printf "\n${yellow}Filesystem:${normal}\n"
 df -h
-printf "\n${yellow}Installing the Tools:${NC}\n"
+printf "\n${yellow}Installing the Tools:${normal}\n"
 opkg update
 opkg upgrade
 opkg install kmod-usb-storage-extras e2fsprogs kmod-fs-ext4
 
-printf "\n${red_bold}Formatting the Device:${NC}\n"
+printf "\n${red_bold}Formatting the Device:${normal}\n"
 umount /tmp/mounts/SD-P1/
 mkfs.ext4 /dev/mmcblk0p1
 
-printf "\n${yellow}Mounting the External Storage Device:${NC}\n"
+printf "\n${yellow}Mounting the External Storage Device:${normal}\n"
 mkdir /mnt/mmcblk0p1
 mount /dev/mmcblk0p1 /mnt/mmcblk0p1
 
-printf "\n${yellow}Duplicating the /overlay Directory:${NC}\n"
+printf "\n${yellow}Duplicating the /overlay Directory:${normal}\n"
 tar -C /overlay -cf - . | tar -C /mnt/mmcblk0p1 -xf -
 umount /mnt/mmcblk0p1/
 
-printf "\n${yellow}Finishing up...${NC}\n"
+printf "\n${yellow}Finishing up...${normal}\n"
 #opkg install block-mount
 block detect > /etc/config/fstab
 sed -i '/enabled/s/0/1/g' /etc/config/fstab
 sed -i 's/mnt\/mmcblk0p1/overlay/g' /etc/config/fstab
-printf "\n${RED}DONE! Now reboot the system!${NC}\n"
+printf "\n${RED}DONE! Now reboot the system!${normal}\n"
