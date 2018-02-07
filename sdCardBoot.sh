727e6a226d99d8684a87d5e123cc16dbaa16d510
Pi4IoT <36192933+Pi4IoT@users.noreply.github.com>
2018-02-07 20:50:12 +0100

    Add files via upload

---
+++ sdCardBoot.sh
@@ -0,0 +1,37 @@
+#!/bin/bash 
+#
+# Script for prepare booting from an SD Card
+# Pi4IoT - 06/01/2018
+#
+
+yellow='\e[0;33m'
+yellow_bold='\e[0;1;33m'
+green='\e[0;32m'
+red_bold='\e[0;31m'
+normal='\e[0m'
+
+printf "\n${yellow}Filesystem:${normal}\n"
+df -h
+printf "\n${yellow}Installing the Tools:${NC}\n"
+opkg update
+opkg upgrade
+opkg install kmod-usb-storage-extras e2fsprogs kmod-fs-ext4
+
+printf "\n${red_bold}Formatting the Device:${NC}\n"
+umount /tmp/mounts/SD-P1/
+mkfs.ext4 /dev/mmcblk0p1
+
+printf "\n${yellow}Mounting the External Storage Device:${NC}\n"
+mkdir /mnt/mmcblk0p1
+mount /dev/mmcblk0p1 /mnt/mmcblk0p1
+
+printf "\n${yellow}Duplicating the /overlay Directory:${NC}\n"
+tar -C /overlay -cf - . | tar -C /mnt/mmcblk0p1 -xf -
+umount /mnt/mmcblk0p1/
+
+printf "\n${yellow}Finishing up...${NC}\n"
+#opkg install block-mount
+block detect > /etc/config/fstab
+sed -i '/enabled/s/0/1/g' /etc/config/fstab
+sed -i 's/mnt\/mmcblk0p1/overlay/g' /etc/config/fstab
+printf "\n${RED}DONE! Now reboot the system!${NC}\n"
